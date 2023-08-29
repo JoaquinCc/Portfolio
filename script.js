1b549a5f10a13d32ab2017d20941e60e014c64df
joaquinfc <joaquinfc357@gmail.com>
2023-08-29 18:43:00 -0500

    version 0.1

---
+++ script.js
@@ -0,0 +1,22 @@
+// funcion quitar menu previo
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    link.className = "seleccionado";
+
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+// funcion para menu responsivo
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
